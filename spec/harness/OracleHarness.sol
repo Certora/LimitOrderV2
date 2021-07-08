9da6c518d293d088329dbf483d05a01510b22692
Shelly Grossman <shellygr151@gmail.com>
2021-07-08 16:31:49 +0300

    working oracle harness for getting optimistic retsize to work

---
+++ spec/harness/OracleHarness.sol
@@ -0,0 +1,42 @@
+pragma solidity 0.6.12;
+
+import "../../contracts/interfaces/IOracle.sol";
+
+contract OracleHarness is IOracle {
+    /// @notice Get the latest exchange rate.
+    /// @param data Usually abi encoded, implementation specific data that contains information and arguments to & about the oracle.
+    /// For example:
+    /// (string memory collateralSymbol, string memory assetSymbol, uint256 division) = abi.decode(data, (string, string, uint256));
+    /// @return success if no valid (recent) rate is available, return false else true.
+    /// @return rate The rate of the requested asset / pair / pool.
+    function get(uint data) override external returns (bool success, uint256 rate) { }
+
+    /// @notice Check the last exchange rate without any state changes.
+    /// @param data Usually abi encoded, implementation specific data that contains information and arguments to & about the oracle.
+    /// For example:
+    /// (string memory collateralSymbol, string memory assetSymbol, uint256 division) = abi.decode(data, (string, string, uint256));
+    /// @return success if no valid (recent) rate is available, return false else true.
+    /// @return rate The rate of the requested asset / pair / pool.
+    function peek(bytes calldata data) override external view returns (bool success, uint256 rate) { }
+
+    /// @notice Check the current spot exchange rate without any state changes. For oracles like TWAP this will be different from peek().
+    /// @param data Usually abi encoded, implementation specific data that contains information and arguments to & about the oracle.
+    /// For example:
+    /// (string memory collateralSymbol, string memory assetSymbol, uint256 division) = abi.decode(data, (string, string, uint256));
+    /// @return rate The rate of the requested asset / pair / pool.
+    function peekSpot(bytes calldata data) override external view returns (uint256 rate) { }
+
+    /// @notice Returns a human readable (short) name about this oracle.
+    /// @param data Usually abi encoded, implementation specific data that contains information and arguments to & about the oracle.
+    /// For example:
+    /// (string memory collateralSymbol, string memory assetSymbol, uint256 division) = abi.decode(data, (string, string, uint256));
+    /// @return s (string) A human readable symbol name about this oracle.
+    function symbol(bytes calldata data) override external view returns (string memory s) { }
+
+    /// @notice Returns a human readable name about this oracle.
+    /// @param data Usually abi encoded, implementation specific data that contains information and arguments to & about the oracle.
+    /// For example:
+    /// (string memory collateralSymbol, string memory assetSymbol, uint256 division) = abi.decode(data, (string, string, uint256));
+    /// @return s (string) A human readable name about this oracle.
+    function name(bytes calldata data) override external view returns (string memory s) { }
+}
--- spec/scripts/runRule.sh
+++ spec/scripts/runRule.sh
@@ -1,6 +1,7 @@
-certoraRun spec/harness/StopLimitOrderHarness.sol spec/harness/SimpleBentoBox.sol spec/harness/SimpleOrderReceiver.sol spec/harness/DummyERC20A.sol spec/harness/DummyERC20B.sol spec/harness/Simplifications.sol \
+certoraRun spec/harness/StopLimitOrderHarness.sol spec/harness/SimpleBentoBox.sol spec/harness/SimpleOrderReceiver.sol spec/harness/DummyERC20A.sol spec/harness/DummyERC20B.sol spec/harness/Simplifications.sol spec/harness/OracleHarness.sol \
     --solc solc6.12 \
     --verify StopLimitOrderHarness:spec/stopLimitOrder.spec \
+    --cache SushiLimitOrder \
     --link StopLimitOrderHarness:bentoBox=SimpleBentoBox \
     --link SimpleOrderReceiver:bentoBox=SimpleBentoBox \
     --settings -enableStorageAnalysis=true,-ignoreViewFunctions,-assumeUnwindCond,-postProcessCounterExamples=true,-optimisticReturnsize=true \
