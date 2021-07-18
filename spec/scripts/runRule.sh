cf2f57c6376638b659b2f470c1a5dd71a013b814
Nurit Dor <nurit@certora.com>
2021-07-18 18:45:12 +0300

    cleanup

--- spec/scripts/runRule.sh
+++
@@ -1,9 +0,0 @@
-certoraRun spec/harness/StopLimitOrderHarness.sol spec/harness/SimpleBentoBox.sol spec/harness/SimpleOrderReceiver.sol spec/harness/DummyERC20A.sol spec/harness/DummyERC20B.sol spec/harness/Simplifications.sol spec/harness/OracleHarness.sol \
-    --solc solc6.12 \
-    --verify StopLimitOrderHarness:spec/stopLimitOrder.spec \
-    --cache SushiLimitOrder \
-    --link StopLimitOrderHarness:bentoBox=SimpleBentoBox \
-    --link SimpleOrderReceiver:bentoBox=SimpleBentoBox \
-    --settings -enableStorageAnalysis=true,-ignoreViewFunctions,-assumeUnwindCond,-postProcessCounterExamples=true,-optimisticReturnsize=true \
-    --rule $1  \
-    --msg "StopLimitOrder : $1 - $2" --staging shelly/robustnessAndCalldatasize
--- spec/scripts/runRuleSanity.sh
+++
@@ -1,7 +0,0 @@
-certoraRun spec/harness/StopLimitOrderHarness.sol spec/harness/SimpleBentoBox.sol spec/harness/SimpleOrderReceiver.sol \
-    --solc solc6.12 \
-    --verify StopLimitOrderHarness:spec/stopLimitOrder.spec \
-    --link StopLimitOrderHarness:bentoBox=SimpleBentoBox \
-    --link SimpleOrderReceiver:bentoBox=SimpleBentoBox \
-    --settings -ruleSanityChecks,-enableStorageAnalysis=true,-ignoreViewFunctions,-assumeUnwindCond,-patient=30,-rule=$1 \
-    --staging --msg "StopLimitOrder Sanity $1 $2"
